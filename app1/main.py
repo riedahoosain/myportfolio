3dc44aea0f7412f857c27a9341fd08c92ba085f0
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-11-19 19:56:25 +0200

    Update main.py

--- app1/main.py
+++ app1/main.py
@@ -1,9 +1,5 @@
 # Accept User input
-todo1 = input("Enter a todo:")
-todo2 = input("Enter a todo:")
-todo3 = input("Enter a todo:")
-
-# Store into list
-todoList = [todo1, todo2, todo3]
-print(todoList)
-
+while True:
+    todo = input("Enter a todo:")
+    print(todo)
+    print("Next...")
