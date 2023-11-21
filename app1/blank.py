b4284dace2f96410b8e4555804e3b88cac0257f0
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-11-21 21:52:08 +0200

    Update blank.py

--- app1/blank.py
+++ app1/blank.py
@@ -1,7 +1,3 @@
 #This is just a temp space I will add or test things before adding to my programs
-
-elements = ['a', 'b', 'c']
-new = 'x'
-elements[1] = new
-print(elements)
+#This is also use to scribble temp info and code
 
