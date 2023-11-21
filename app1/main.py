dd11d13f122e84dc55fc9d6e7366c1e6ef525618
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-11-21 21:22:01 +0200

    Update main.py
    sorted todo list

--- app1/main.py
+++ app1/main.py
@@ -8,25 +8,30 @@ print("You can add, edit, complete, show a todo or exit program")
 # Accept User input
 while True:   
     user_action = input("Type add, edit, show, complete or exit: ")
+    user_action = user_action.strip()
 
     match user_action:
         case 'add'| 'a':
             todo = input("Enter a todo: ")
             todos.append(todo)
+            todos.sort()
 
         case 'edit' | 'e':
             number = int(input("Number of the todo to edit: "))
+            number = number-1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
 
         case 'show' | 's':
             print("List of todos")
+            todos.sort()
             for index, item in enumerate(todos):
-                print(f"{index}: {item}")
+                print(f"{index + 1}: {item}")
+            print(f"Total items to do is: {len(todos)}")
 
         case 'complete' | 'c':
             number = int(input("Number of the todo to complete: "))
-            todos.pop(number)            
+            todos.pop(number - 1)            
 
         case 'exit':
             break
