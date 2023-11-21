644a3de6cd28810dd30bb9fe81187b20709e7c75
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-11-21 20:35:47 +0200

    Update main.py

--- app1/main.py
+++ app1/main.py
@@ -2,26 +2,31 @@
 
 todos = []
 print("Welcome to the Todo List system")
-print("You can add, edit, show a todo or exit program")
+print("You can add, edit, complete, show a todo or exit program")
 
 
 # Accept User input
 while True:   
-    user_action = input("Type add, edit, show, or exit: ")
+    user_action = input("Type add, edit, show, complete or exit: ")
 
     match user_action:
-        case 'add':
+        case 'add'| 'a':
             todo = input("Enter a todo: ")
             todos.append(todo)
 
-        case 'edit':
+        case 'edit' | 'e':
             number = int(input("Number of the todo to edit: "))
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
 
-        case 'show':
+        case 'show' | 's':
+            print("List of todos")
             for index, item in enumerate(todos):
                 print(f"{index}: {item}")
+
+        case 'complete' | 'c':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number)            
 
         case 'exit':
             break
