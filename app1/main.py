7e72b36df8b281a88cab9e32dffa61f963c783ec
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-11-27 22:04:01 +0200

    changes to conditional
    Changed Case to IF-THEN

--- app1/main.py
+++ app1/main.py
@@ -8,71 +8,68 @@ print("You can add, edit, complete, show a todo or exit program")
 while True:   
     user_action = input("Type add, edit, show, complete or exit: ")
     user_action = user_action.strip()
-
-
-
-    match user_action:
+    
+    if 'add' in user_action:    
+    
+        todo = input("Enter a todo: ") + "\n"
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines() #Save File to List
         
-        case 'add'| 'a':
-            todo = input("Enter a todo: ") + "\n"
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines() #Save File to List
-           
 
 
-            todos.append(todo)
-            with open('files/todos.txt', 'w') as file:
+        todos.append(todo)
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos) #Save List to File with added items
+        
+
+    if 'edit' in user_action:    
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
+
+        
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines() #Save File to List     
+
+        new_todo = input("Enter new todo: ")
+        todos[number] = new_todo + '\n'
+
+
+        with open('files/todos.txt', 'w') as file:
                 file.writelines(todos) #Save List to File with added items
-            
-
-        case 'edit' | 'e':
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
-
-          
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines() #Save File to List     
-
-            new_todo = input("Enter new todo: ")
-            todos[number] = new_todo + '\n'
-
-
-            with open('files/todos.txt', 'w') as file:
-                 file.writelines(todos) #Save List to File with added items
 
 
 
-        case 'show' | 's':
-            print("List of todos")
-            
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+    if 'show' in user_action:
+        print("List of todos")
+        
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            #new_todos =[item.strip('\n') for item in todos] #List Comprehension. a For Loop written in one short line            
+        #new_todos =[item.strip('\n') for item in todos] #List Comprehension. a For Loop written in one short line            
 
-            for index, item in enumerate(todos):
-                print(f"{index + 1}: {item}", end="")
-            print('\n')        
-            print(f"Total items to do is: {len(todos)}")
+        for index, item in enumerate(todos):
+            print(f"{index + 1}: {item}", end="")
+        print('\n')        
+        print(f"Total items to do is: {len(todos)}")
 
 
 
-        case 'complete' | 'c': #Removes the Todo from the file since it has been completed
+    if 'complete' in user_action: #Removes the Todo from the file since it has been completed
 
-            number = int(input("Number of the todo to complete: "))
+        number = int(input("Number of the todo to complete: "))
 
-            with open('files/todos.txt', 'r') as file:
-                 todos = file.readlines() #Save File to List  
-            index = (number - 1)
-            todo_to_remove = todos[index].strip('\n')        
+        with open('files/todos.txt', 'r') as file:
+                todos = file.readlines() #Save File to List  
+        index = (number - 1)
+        todo_to_remove = todos[index].strip('\n')        
 
-            todos.pop(index) #Remove item from the todos list
+        todos.pop(index) #Remove item from the todos list
 
-            with open('files/todos.txt', 'w') as file:
-                 file.writelines(todos) #Save List to File with removed items
-            print(f"Todo {todo_to_remove} was removed from the list")             
+        with open('files/todos.txt', 'w') as file:
+                file.writelines(todos) #Save List to File with removed items
+        print(f"Todo {todo_to_remove} was removed from the list")             
 
-        case 'exit':
-            break
+    if 'exit' in user_action:
+        break
         
 print("Thank for you using the Todo List System")   
