bd3ae5b8e5765c768681de31fd5a9e3cd4a9ae2d
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-04 22:51:24 +0200

    decoupling
    decoupled some functions

--- app1 - todo list/main.py
+++ app1 - todo list/main.py
@@ -2,6 +2,7 @@
 
 # Function to read todos from the file
 def get_todos(filepath="files/todos.txt"):
+    """Reads todos text file and saves to the list """
     try:
         with open(filepath, 'r') as file:
             todos_local = file.readlines()  # Save File to List
@@ -12,6 +13,7 @@ def get_todos(filepath="files/todos.txt"):
 
 # Function to write todos to the file
 def write_todos(todos_arg, filepath="files/todos.txt"):
+    """Writes the list to todos text file """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)  # Save List to File with added items
 
--- feetinches/feetinches.py
+++ feetinches/feetinches.py
@@ -1,15 +1,23 @@
 feet_inches = input("Enter feet and inches: ")
 
-
-def convert(feet_inches):
+def parse(feet_inches):
     parts = feet_inches.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
+    return {"feet": feet, "inches": inches}
+
+
+
+def convert(feet, inches):    
 
     meters = feet * 0.3048 + inches * 0.0254
     return meters
 
-result = convert(feet_inches)
+parsed = parse(feet_inches)
+
+result = convert(parsed['feet'], parsed['inches'])
+
+print(f"{parsed['feet']} feet and {parsed['inches']} inches is equal to {result}m")
 
 if result < 1:
     print(f"Kid is {result}m and is too short to use the slide.")
