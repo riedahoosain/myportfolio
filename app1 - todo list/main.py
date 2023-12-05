1fc0b9e9a76035adffd5e1ec4ebc85f8eb1453a5
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-05 13:01:51 +0200

    minor changes
    Some small changes to make code readable
    import complete functions module

--- app1 - todo list/main.py
+++ app1 - todo list/main.py
@@ -1,25 +1,28 @@
 # Todo List System
-from functions import get_todos, write_todos
+# This App alows a user to create a list of Todos that is written or read from a text file
+# Users can also edit or complete todos
+
+# from functions import get_todos, write_todos
+
+# All functions sits in this module
+# get_todos and write_todos sits here
+import functions
 
 
 # This Function adds a todo
 def add_to_todo(user_action_local):
     todo = user_action_local[4:] + "\n"
-
-    todos = get_todos()
+    todos = functions.get_todos()
     todos.append(todo)
+    functions.write_todos(todos)
 
-    write_todos(todos)
+# This Function shows the todos
 
-# This function shows list of todos
+
 def show_to_do():
 
     print("List of todos")
-
-    todos = get_todos()
-
-    # new_todos =[item.strip('\n') for item in todos] #List Comprehension. a For Loop written in one short line
-
+    todos = functions.get_todos()
     for index, item in enumerate(todos):
         print(f"{index + 1}: {item}", end="")
     print('\n')
@@ -30,26 +33,20 @@ def show_to_do():
 def edit_to_do(user_action_local):
     number = int(user_action_local[5:])
     number = number - 1
-    todos = get_todos()
-
+    todos = functions.get_todos()
     new_todo = input("Enter new todo: ")
     todos[number] = new_todo + '\n'
-
-    write_todos(todos)
+    functions.write_todos(todos)
 
 
 # This function completes a todo and removes from the file
 def complete_to_do(user_action_local):
     number = int(user_action[9:])
-    todos = get_todos()
-
+    todos = functions.get_todos()
     index = (number - 1)
     todo_to_remove = todos[index].strip('\n')
-
     todos.pop(index)  # Remove item from the todos list
-
-    write_todos(todos)
-
+    functions.write_todos(todos)
     print(f"Todo {todo_to_remove} was removed from the list")
 
 
