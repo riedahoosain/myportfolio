b184f63ac4711ec6f7fc1996bd3e77d2820d5ee8
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-05 11:33:59 +0200

    split functions
    Function were split from main code

--- app1 - todo list/main.py
+++ app1 - todo list/main.py
@@ -1,21 +1,5 @@
 # Todo List System
-
-# Function to read todos from the file
-def get_todos(filepath="files/todos.txt"):
-    """Reads todos text file and saves to the list """
-    try:
-        with open(filepath, 'r') as file:
-            todos_local = file.readlines()  # Save File to List
-            return todos_local
-    except FileNotFoundError:
-        print("File or Dir not found")
-
-
-# Function to write todos to the file
-def write_todos(todos_arg, filepath="files/todos.txt"):
-    """Writes the list to todos text file """
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)  # Save List to File with added items
+from functions import get_todos, write_todos
 
 
 # This Function adds a todo
@@ -28,9 +12,8 @@ def add_to_todo(user_action_local):
     write_todos(todos)
 
 # This function shows list of todos
-
-
 def show_to_do():
+
     print("List of todos")
 
     todos = get_todos()
