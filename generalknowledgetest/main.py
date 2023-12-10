fb4d2d483835022bb014620bdb499a70876f76a2
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-10 12:53:45 +0200

    bug fix
    fixed a bug where score was showing incorrect even though correct answers were provided

--- generalknowledgetest/main.py
+++ generalknowledgetest/main.py
@@ -1,10 +1,20 @@
+'''
+A Questions and Answers app that reads from json file 
+that asks user multiple choice questions and shows scores and answers
+
+The File can have more multiple questions
+
+'''
+
 import json
+
 
 def extractdata():
     with open("questions.json", 'r') as file:
         content = file.read()
     data_local = json.loads(content)
     return data_local
+
 
 data = extractdata()
 score = 0
@@ -14,15 +24,17 @@ for question in data:
         print(f"{index + 1} - {alternative}")
     user_choice = int(input("Enter your answer: "))
     question["user_choice"] = user_choice
-    if user_choice == question["correct_answer"]:
-        print("correct answer")
-        score = score + 1
-    else:
-        print("incorrect answer")
+
 
 for index, question in enumerate(data):
-    message = f"{index + 1} - Your answer: {question['user_choice']}, " \
-              f"Correct Answer: {question['correct_answer']}"
+    if question["user_choice"] == question["correct_answer"]:
+        result = "Correct answer"
+        score = score + 1
+    else:
+        result = "Incorrect answer"
+
+    message = f"{index + 1} {result} - Your answer: {question['user_choice']}, " \
+        f"Correct Answer: {question['correct_answer']}"
     print(message)
-#print(data)
+# print(data)
 print(f"{score}  /  {len(data)}")
