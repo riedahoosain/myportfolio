e1883077803c9c2fcb77350f88d45fba54a41c27
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-10 22:15:12 +0200

    create zip program
    Create a Zip program that has a GUI interface

--- app1 - todo list/gui.py
+++ app1 - todo list/gui.py
@@ -7,6 +7,7 @@ import PySimpleGUI as sg
 label = sg.Text("Type in todo")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+#window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window = sg.Window('My To-Do App', layout=[[label, input_box, add_button]])
 window.read()
 window.close()
---
+++ zipper/main.py
@@ -0,0 +1,17 @@
+#This app zips a list of files
+
+import PySimpleGUI as sg
+
+
+label1 = sg.Text("Select files to compress")
+input1 = sg.Input()
+choose_button1 = sg.FileBrowse("Choose")
+
+label2 = sg.Text("Select destination folder")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose")
+
+
+window = sg.Window('My Zipper App', layout=[[label1, input1, choose_button1], [label2, input2, choose_button2]])
+window.read()
+window.close()
