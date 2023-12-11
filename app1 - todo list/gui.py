187cd4d84c7c545a15a1cd0631177cbddccbfe82
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-11 14:29:06 +0200

    change theme added clock
    Changed theme, added a clock, added error handling

--- app1 - todo list/gui.py
+++ app1 - todo list/gui.py
@@ -2,6 +2,7 @@
 
 import functions
 import PySimpleGUI as sg
+import time
 
 
 def add_to_todo():
@@ -10,51 +11,73 @@ def add_to_todo():
     '''
     try:
         todos = functions.get_todos()
-        new_todo = values['todo'] + "\n"
+        new_todo = values['todo'].strip() + "\n"
         todos.append(new_todo)
         functions.write_todos(todos)
         window['todos'].update(values=todos)
+        window['todo'].update(value="")
+
     except AttributeError:
         print("The was an error and the Todo was not added")
         print("This can happen if the file that writes the todo is not found")
 
 
 def edit_to_do():
-    todo_to_edit = values['todos'][0]
-    new_todo = values['todo']
+    try:
+        todo_to_edit = values['todos'][0]
+        new_todo = values['todo']
 
-    todos = functions.get_todos()
-    index = todos.index(todo_to_edit)
-    todos[index] = new_todo + '\n'
-    functions.write_todos(todos)
-    window['todos'].update(values=todos)
+        todos = functions.get_todos()
+        index = todos.index(todo_to_edit)
+        todos[index] = new_todo + '\n'
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+
+    except IndexError:
+        window['information'].update(
+            value="Please select an item first before you Edit")
+        sg.popup("Please select an item first before you Edit",
+                 font=('Helvetica', 20))
+
 
 def complete_to_do():
-    todo_to_complete = values['todos'][0]
-    todos = functions.get_todos()
-    todos.remove(todo_to_complete)
-    functions.write_todos(todos)
-    window['todos'].update(values=todos)
-    window['todo'].update(value="")
+    try:
+        todo_to_complete = values['todos'][0]
+        todos = functions.get_todos()
+        todos.remove(todo_to_complete)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        window['todo'].update(value="")
+    except IndexError:
+        window['information'].update(
+            value="Please select an item first before you click Complete")
+        sg.popup("Please select an item first before you click Complete",
+                 font=('Helvetica', 20))
+
 
 def create_gui():
     '''
     This creates the GUI Interface
     '''
+    sg.theme("Black")
 
+    clock = sg.Text("", key="clock")
     label = sg.Text("Type in todo")
     input_box = sg.InputText(tooltip="Enter todo", key="todo")
-    add_button = sg.Button("Add")
-    exit_button = sg.Button("Exit")
+    add_button = sg.Button("Add")    
     edit_button = sg.Button("Edit")
     complete_button = sg.Button("Complete")
+    exit_button = sg.Button("Exit")
     list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                           enable_events=True, size=[45, 10])
+    info_text = sg.Text("ℹ️", key="information")
 
-    menu_layout = [[label],
-                   [input_box, add_button],
-                   [list_box, edit_button, complete_button],
-                   [exit_button]]
+    menu_layout = [[clock],
+                   [label],
+                   [input_box],
+                   [list_box,],
+                   [info_text],
+                   [add_button, edit_button, complete_button, exit_button]]
     # Stores the GUI Layout in a list which is then used when loading GUI
 
     window = sg.Window('My To-Do App',
@@ -65,32 +88,28 @@ def create_gui():
 
 window = create_gui()
 while True:
-    event, values = window.read()
-    
-    #print(event)
-    #print(values)
-    #print(values['todos'])
-    
+    event, values = window.read(timeout=1000)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))  
+
     match event:
         case "Add":
-            add_to_todo()
-       
+            if values['todo'].strip() == "":
+                window['information'].update(value="Please enter a todo before adding")
+                sg.popup("Please enter a todo before adding", font=('Helvetica', 20))
+            else:
+                add_to_todo()
+
         case "Edit":
             edit_to_do()
 
         case "Complete":
             complete_to_do()
-          
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
         case "Exit" | sg.WIN_CLOSED:
-            break
-
-        #case sg.WIN_CLOSED:
-         #   break
-
+            break       
 
 window.close()
 print("Good Bye?")
