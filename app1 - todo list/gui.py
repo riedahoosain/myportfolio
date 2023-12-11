9fc0ed5b76ae744e30864139ff16ac61f817be4e
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-11 13:27:28 +0200

    implement complete
    Implemented the Complete Button
    Made some changes to exit by merge the exit button and clicking the X

--- app1 - todo list/gui.py
+++ app1 - todo list/gui.py
@@ -3,31 +3,39 @@
 import functions
 import PySimpleGUI as sg
 
+
 def add_to_todo():
-        '''
-        Adds 
-        '''
-        try:
-            todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        except AttributeError:
-            print("The was an error and the Todo was not added")
-            print("This can happen if the file that writes the todo is not found")
-
-def edit_to_do():
-        todo_to_edit = values['todos'][0]
-        new_todo = values['todo']
-
+    '''
+    Adds a todo to the list
+    '''
+    try:
         todos = functions.get_todos()
-        index = todos.index(todo_to_edit)
-        todos[index] = new_todo + '\n'
+        new_todo = values['todo'] + "\n"
+        todos.append(new_todo)
         functions.write_todos(todos)
         window['todos'].update(values=todos)
+    except AttributeError:
+        print("The was an error and the Todo was not added")
+        print("This can happen if the file that writes the todo is not found")
 
 
+def edit_to_do():
+    todo_to_edit = values['todos'][0]
+    new_todo = values['todo']
+
+    todos = functions.get_todos()
+    index = todos.index(todo_to_edit)
+    todos[index] = new_todo + '\n'
+    functions.write_todos(todos)
+    window['todos'].update(values=todos)
+
+def complete_to_do():
+    todo_to_complete = values['todos'][0]
+    todos = functions.get_todos()
+    todos.remove(todo_to_complete)
+    functions.write_todos(todos)
+    window['todos'].update(values=todos)
+    window['todo'].update(value="")
 
 def create_gui():
     '''
@@ -39,40 +47,50 @@ def create_gui():
     add_button = sg.Button("Add")
     exit_button = sg.Button("Exit")
     edit_button = sg.Button("Edit")
-    list_box = sg.Listbox(values=functions.get_todos(), key='todos', 
-                          enable_events=True, size=[45,10])
-  
+    complete_button = sg.Button("Complete")
+    list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                          enable_events=True, size=[45, 10])
 
-    menu_layout = [[label], [input_box, add_button],[list_box, edit_button],[exit_button]]
-    #Stores the GUI Layout in a list which is then used when loading GUI
-
+    menu_layout = [[label],
+                   [input_box, add_button],
+                   [list_box, edit_button, complete_button],
+                   [exit_button]]
+    # Stores the GUI Layout in a list which is then used when loading GUI
 
     window = sg.Window('My To-Do App',
-                    layout=menu_layout,
-                    font=('Helvetica', 20))
+                       layout=menu_layout,
+                       font=('Helvetica', 20))
     return window
+
+
 window = create_gui()
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
-    print(values['todos'])
+    
+    #print(event)
+    #print(values)
+    #print(values['todos'])
+    
     match event:
         case "Add":
             add_to_todo()
-           
-        case sg.WIN_CLOSED:
-            break
-
+       
         case "Edit":
-            edit_to_do()        
+            edit_to_do()
+
+        case "Complete":
+            complete_to_do()
+          
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
-        case "Exit":
-            print("Good Bye?")
-            exit()
-         
+        case "Exit" | sg.WIN_CLOSED:
+            break
+
+        #case sg.WIN_CLOSED:
+         #   break
+
+
 window.close()
 print("Good Bye?")
