6942841b1c2768f6772476a5fc8dd7f7f975e73d
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-12 22:57:54 +0200

    implement add new todo
    Implemented Add new To Do feature

--- app1 - todo list/web.py
+++ app1 - todo list/web.py
@@ -8,11 +8,41 @@ import functions  # All functions sits in this module # get_todos and write_todo
 import time
 import os
 
+# Lists all the todos in the file
+def show_to_do():
+    todos = functions.get_todos()
+    for item in todos:
+        st.checkbox(item)
+    st.write('\n')
+    if len(todos) == 0:
+        st.write("List is empty")
+    else:
+        st.write(f"Total items to do is: {len(todos)}")
+
+
+
+#Adds a new to do
+def add_to_do():
+    '''
+    Adds the todo
+    '''
+    todos = functions.get_todos()
+    todo = st.session_state['new_todo']   
+    todo = todo + "\n"    
+    todos.append(todo)
+    functions.write_todos(todos)
+
+    print(todos)
+
+
+# MAIN PROGRAM STARTS HERE:
 
 st.set_page_config(page_title="My ToDo App", page_icon="icon.png")
 st.title("My Todo App")
 st.subheader("This app is used to create a todo or any list for that matter")
 st.subheader("List of todos")
+
+show_to_do()
 
 # Checks if the todo file exists which is stored in constant variable FILEPATH
 # Creates a new file it does not exist
@@ -20,13 +50,6 @@ if not os.path.exists(functions.FILEPATH):
     with open(functions.FILEPATH, 'w') as file:
         pass
 
-todos = functions.get_todos()
-for item in todos:
-    st.checkbox(item)
-st.write('\n')
-if len(todos) == 0:
-    st.write("List is empty")
-else:
-    st.write(f"Total items to do is: {len(todos)}")
 
-st.text_input(label="Add todo", placeholder="Add a new to do", label_visibility="hidden")
+st.text_input(label="Add todo", placeholder="Add a new to do", 
+            label_visibility="hidden",on_change=add_to_do, key='new_todo')
