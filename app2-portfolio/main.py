28887b24eb16a40dd15ac377c98364708f24ea23
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-13 17:20:01 +0200

    extracted list of apps to websites with pandas
    Render the app titles into the columns

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas
 
-st.set_page_config(layout='wide', page_title="Rieda's Portfolio", page_icon='images/20.png')
+st.set_page_config(
+    layout='wide', page_title="Rieda's Portfolio", page_icon='images/20.png')
 
 
 col1, col2 = st.columns(2)
@@ -27,3 +29,15 @@ contactme = '''
 Below is some of the apps that I have built while learning Python. Feel Free to contact me if you have any questions
 '''
 st.write(contactme)
+
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv('data.csv', sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
