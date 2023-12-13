a54d2a931ad83170069f981041a4722c13728f37
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-13 17:36:15 +0200

    Added app description, images and links to webpage
    Added app description, images and links to webpage

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -30,14 +30,20 @@ Below is some of the apps that I have built while learning Python. Feel Free to 
 '''
 st.write(contactme)
 
-
-col3, col4 = st.columns(2)
+#Uses Pandas to pull data from CSV that contains Apps, and Source to Apps Built
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv('data.csv', sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.link_button(label='Source Code', url=row["url"])
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.link_button(label='Source Code', url=row["url"])
