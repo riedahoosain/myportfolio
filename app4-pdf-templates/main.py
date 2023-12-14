8dcd7a8c61e5e38a399f9ba4c40f5fc049bf3bee
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-14 22:27:17 +0200

    Add more pages for each topic
    Add more pages for each topic

--- app4-pdf-templates/main.py
+++ app4-pdf-templates/main.py
@@ -2,15 +2,24 @@
 from fpdf import FPDF #PDF module
 import pandas as pd #Works nicely for CSV and data files
 
+#Creates a PDF instance
 pdf = FPDF(orientation="P", unit="mm",format="A4")
 
+#Loads CSV File
 df = pd.read_csv("topics.csv")
+#Loops thorough the data creating pages for each info based on number pages needed
+#The data contains the amount pages needed
+
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(0, 100, 0)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10,21,200,21)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+       
 pdf.output("output.pdf")
 print("File was created")
 
