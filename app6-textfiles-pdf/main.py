e579fa5fb4b98777b961a365f6d0d8c6a2e06aff
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-15 09:04:29 +0200

    Created an app that reads  folder of text files and sends all data to single PDF file
    Created an app that reads  folder of text files and sends all data to single PDF file

---
+++ app6-textfiles-pdf/main.py
@@ -0,0 +1,45 @@
+# Reads text files in files/ and then extracts data to multipage PDF file
+
+from fpdf import FPDF
+import glob
+from pathlib import Path
+
+filepaths = glob.glob("files/*.txt")
+
+# Creates PDF instance
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+
+for filepath in filepaths:
+    # Adds a new page for each filename
+    pdf.add_page()
+
+    # extracts file name minus extenstions
+    filenames = Path(filepath).stem
+
+    with open(filepath, 'r') as file:
+        data = file.read()
+
+    # Writes the the first filename to the instance
+    # Converts to title case eg Cats instead of cats
+    pdf.set_font(family="Times", style="B", size=20)
+    pdf.cell(w=50, h=8, txt=filenames.title(), ln=1)
+    pdf.set_font(family="Times", size=10)
+    pdf.multi_cell(200, 10, data)
+
+# Saves instance with all data to one PDF file
+# Saves to the same folder where main file is
+try:
+    pdf.output("output.pdf")
+    print("""
+    ###############################
+    PDF file output.pdf was created.
+    ###############################
+    """)
+except PermissionError:
+    print("""
+    ####################
+    File was not created.
+    Please make sure that output.pdf is not open at the moment.
+    ####################
+          """)
