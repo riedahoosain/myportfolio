605231306475caa713377d4066191f4db3a2ea80
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-15 11:54:26 +0200

    Add header, total price, company and logo
    Add header, total price, company and logo

--- app5-pdf-invoice-generator/main.py
+++ app5-pdf-invoice-generator/main.py
@@ -46,6 +46,30 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["total_price"]),border=1, ln=1)
 
 
+    
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="",border=1)
+    pdf.cell(w=30, h=8, txt="",border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add Total sum sentence
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # Company name and logo
+    pdf.image("logo.png", w=5)
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt="Innovative Technology")
+
+
+
+
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 
