13e13e0541939688269709cdcabef5a040c2a749
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-15 22:02:26 +0200

    Update main.py
    Add links, specify language, limit news, add subject, control category

--- app7-news-api-email/main.py
+++ app7-news-api-email/main.py
@@ -1,23 +1,33 @@
 # A website that gets data from a news API and uses sentiment analysis to publish only positive news
 import requests
 from send_email import send_email
+
 api_key = "5813e71c54cd49acb1cfcbfb63e75770"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "sortBy=publishedAt&apiKey=" \
-      "5813e71c54cd49acb1cfcbfb63e75770"
+language = "en"
+topic = "tesla"
+
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      "apiKey=" \
+      f"{api_key}&" \
+      f"language={language}"
+
 # Make request
 request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
-# Access the article titles and description
+# Access 20 article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][0:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+        body = body + article["title"] + "\n" \
+        + str(article["description"]) \
+        + "\n" + article["url"]+ 2*"\n"
 
 # Create Email Body to be sent and run email send function
-body = "Subject: Tesla news from News API App \n" + body
+body = "Subject: Today's news from News API App \n" + body
 body = body.encode("utf-8")
 
 send_email(message=body)
-print("Email Sent")
+print("Email Articles Sent")
