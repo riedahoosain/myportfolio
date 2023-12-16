068d88926a7a235a036443c4202066d7a94c7f7b
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-16 09:42:06 +0200

    Initial Commit
    Initial Commit

--- app9-weather-api/main.py
+++ app9-weather-api/main.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask("My Website")
+app = Flask(__name__)
+
 
 @app.route("/")
-def mainpage():
-    return render_template("home.html")
-
-@app.route("/about")
-def about1():
-    return render_template("about.html")
-
-@app.route("/about/")
-def about2():
-    return render_template("about.html")
-
-@app.route("/home")
 def home():
     return render_template("home.html")
 
-app.run(debug=True)
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+if __name__ == "__main__":
+    app.run(debug=True)
