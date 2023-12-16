4d271f64aefffb033ae61c7c6eff8f6f03964cd5
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-16 19:50:05 +0200

    Add URL for yearly data
    Added URL endpoint for yearly data

--- app10-dictionary-api/main.py
+++ app10-dictionary-api/main.py
@@ -1,32 +1,39 @@
+# This API will provide a dictionary meaning of a word when provided
 from flask import Flask, render_template
 import pandas as pd
+FILEPATH = "dictionary.csv"
 
 app = Flask(__name__)
 
-df = pd.read_csv("dictionary.csv")
+df = pd.read_csv(FILEPATH)
 
 
-
+# API Home Page
 @app.route("/")
 def home():
     return render_template("home.html")
-    
 
 
+# API request when providing a word in format http://127.0.0.1:5000/api/v1/[word]
 @app.route("/api/v1/<word>")
 def api(word):
-    
+
     definition = df.loc[df["word"] == word]['definition'].squeeze()
     results = {"word": word, "definition": definition}
-    
+
     return results
+# Returns the dictionary list from the API - http://127.0.0.1:5000/api/
+
 
 @app.route("/api/")
 def all():
-  d = df.to_dict(orient='records')
-  return d
+    d = df.to_dict(orient='records')
+    return d
 
 
 if __name__ == "__main__":
     # Default Port is 5000 but specifying we can change the ports to run multiple apps
+    # use host to provide an IP instead
+    # app.run(debug=True, host="127.0.0.1", port=5000)
+
     app.run(debug=True, port=5000)
--- app9-weather-api/main.py
+++ app9-weather-api/main.py
@@ -2,24 +2,53 @@ from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
+FILEPATH = "data/stations.txt"
 
-stations = pd.read_csv("data/stations.txt",skiprows=17)
-stations = stations[["STAID","STANAME                                 "]]
+stations = pd.read_csv(FILEPATH, skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
+# Loads the home api page
 
 
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
 
+# For one station for one date: http://127.0.0.1:5000/api/v1/10/1988-10-25
+
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
     filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])    
-    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10   
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
+# for one station for all dates: http://127.0.0.1:5000/api/v1/10
+
+
+@app.route("/api/v1/<station>")
+def all_data_per_stations(station):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient='records')
+    return result
+
+# for one station for one year: http://127.0.0.1:5000/api/v1/yearly/10/1988
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def all_data_per_year(station, year):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(
+        str(year))].to_dict(orient='records')
+    return result
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000) #Default Port is 5000 but specifying we can change the ports to run multiple apps
+    # Default Port is 5000 but specifying we can change the ports to run multiple apps
+    app.run(debug=True, port=5000)
