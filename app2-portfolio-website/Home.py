c0f1a05cdd3a624b7c10611cf94c7f90bb5367cd
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-16 09:20:34 +0200

    created website for weather API
    Added Website for API

--- app2-portfolio-website/Home.py
+++ app2-portfolio-website/Home.py
@@ -12,7 +12,8 @@ st.set_page_config(
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    #st.image("images/photo.png")
+    st.image("images/profile.png", width=400)
 
 with col2:
     st.title("Rieda Hoosain")
