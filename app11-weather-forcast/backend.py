0a5c02cb54401cab6b9ff7ae6b274bb0bc7b5007
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-17 15:51:24 +0200

    Completed get_data function
    1. Completed the Function that gets the weather data
    2. Fixed the bug that occurs when user types in an invalid place
    3. Changed weather data to display as degrees C

--- app11-weather-forcast/backend.py
+++ app11-weather-forcast/backend.py
@@ -2,19 +2,14 @@ import requests
 API_KEY = "20842ff75b793afea034fc4aef66393b"
 place = "Cape Town"
 
-def get_data(place, forecast_days=None, data_type=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if data_type == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if data_type == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place, forecast_days=1, data_type="Sky"))
+    print(get_data(place, forecast_days=1))
--- app11-weather-forcast/main.py
+++ app11-weather-forcast/main.py
@@ -6,15 +6,33 @@ from backend import get_data
 
 st.set_page_config(
     layout='centered', page_title="Weather Forecast", page_icon="16.png")
+
+# Add title, text input, slider, selectbox and subheader
 st.header("Weather Forecast for the next days")
 place = st.text_input("Place")
 days = st.slider("Forcast Days", min_value=1, max_value=5,help="Select the number of days of forecasted days")
 option = st.selectbox("Select data to view", 
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+if place:
 
-d,t = get_data(place, days, option)
+    # Get the Temperature or Sky Data
+    try:
+        filtered_data = get_data(place, days)
+
+    
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]            
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x":"Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)    
 
 
-figure = px.line(x=d, y=t, labels={"x":"Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png","Clouds": "images/cloud.png","Rain": "images/rain.png","Snow": "images/snow.png",}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images_path = [images[condition] for condition in sky_conditions]       
+
+            st.image(images_path, width=115)
+    except KeyError:
+        st.info(f"{place} does not exist. Please make sure the place entered is spelt correctly")
