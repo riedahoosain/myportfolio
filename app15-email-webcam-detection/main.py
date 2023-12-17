416610f9262b51d9e2e2296f90537911e5c64dfd
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-17 23:15:10 +0200

    Added video capture
    Added video capture option for webcam app

--- app15-email-webcam-detection/main.py
+++ app15-email-webcam-detection/main.py
@@ -2,8 +2,14 @@
 # A program that monitors the computer webcam and sends an email when a new object enters the view.
 
 import cv2 # this is opencv-python
+import time
 
 video = cv2.VideoCapture(0)
-check, frame = video.read()
-print(check)
-print(frame)
+time.sleep(1)
+while True:    
+    check, frame = video.read()
+    cv2.imshow("My Video", frame)
+    key=cv2.waitKey(1)
+    if key == ord("q"):
+        break
+video.release()
