6f03fab2109526960fd96cc39575c03a138a4087
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-24 19:05:56 +0200

    Added Function to scrape data from URL
    Added function to scrape the data from URL where the event is available

---
+++ app17-scraping-tours-sql/main.py
@@ -0,0 +1,24 @@
+# A program that checks a music band's website and sends an email when there are new tours.
+import requests
+import selectorlib
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+URL = "http://programmer100.pythonanywhere.com/tours/"
+
+def scrape(url):
+    """Scrape the page source from URL"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
