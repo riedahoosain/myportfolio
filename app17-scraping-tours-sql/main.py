c584053e939296f9f3ce4b682e74af7df89c51bb
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-24 23:25:30 +0200

    Implemented send_email function
    Implemented send_email function to send an email when a new even has been found.

--- app17-scraping-tours-sql/main.py
+++ app17-scraping-tours-sql/main.py
@@ -1,6 +1,11 @@
 # A program that checks a music band's website and sends an email when there are new tours.
 import requests
 import selectorlib
+import smtplib
+import smtplib, ssl
+import os
+import time
+
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -20,7 +25,21 @@ def extract(source):
     return value
 
 
-def send_email():
+def send_email(message):
+
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "innotechsa24@gmail.com"
+    password = "deyf zggm ptau giqx"
+
+    receiver = "innotechsa24@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
     print("Email was sent")
 
 
@@ -35,12 +54,26 @@ def read_data(extracted):
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
 
-    content = read_data(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store_data(extracted)
-            send_email()
+    while True:
+
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        content = read_data(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store_data(extracted)            
+                send_email(message = f"""\
+Subject: New event was found
+                       
+Hey, new event was found.
+                       
+Sent from My Webscraping App
+                       
+""")
+        time.sleep(10)
+
+
+             
