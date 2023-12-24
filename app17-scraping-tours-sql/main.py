4aeb2369c4a46f8ebc86996720271e7f36e02d6b
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-24 23:00:02 +0200

    Added option to store scraped data to text file
    Added option to store scraped data to text file

--- app17-scraping-tours-sql/main.py
+++ app17-scraping-tours-sql/main.py
@@ -6,11 +6,13 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 URL = "http://programmer100.pythonanywhere.com/tours/"
 
+
 def scrape(url):
     """Scrape the page source from URL"""
     response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
+
 
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
@@ -18,7 +20,27 @@ def extract(source):
     return value
 
 
+def send_email():
+    print("Email was sent")
+
+
+def store_data(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read_data(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+
+    content = read_data(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store_data(extracted)
+            send_email()
