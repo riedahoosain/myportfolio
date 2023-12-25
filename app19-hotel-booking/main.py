2c125d515cad5fd6dc5fd17923b330fe7e260d4b
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-25 20:01:39 +0200

    Created our Hotel booking system
    Added the Hotel and Reservation class

---
+++ app19-hotel-booking/main.py
@@ -0,0 +1,10 @@
+# This is a Hotel booking system that allows users to book a hotel and make reservations
+
+class Hotel:
+    def book(self):
+        pass
+    
+
+class ReservationTicket:
+    def generate(self):
+        pass
