f86b1cbf67f2922d0de11b46043379f56da4eedb
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-25 18:33:30 +0200

    Added the music band website using classes
    Added the music band website using classes

---
+++ app18-oop-example/main.py
@@ -0,0 +1,103 @@
+# A program that checks a music band's website and sends an email when there are new tours.
+import requests
+import selectorlib
+import smtplib
+import ssl
+import os
+import time
+import sqlite3
+
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+URL = "http://programmer100.pythonanywhere.com/tours/"
+
+
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        '''Extract just the require data from the webpage'''
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
+
+
+class Email:
+    def send(self, message):
+
+        host = "smtp.gmail.com"
+        port = 465
+
+        username = "innotechsa24@gmail.com"
+        password = "deyf zggm ptau giqx"
+
+        receiver = "innotechsa24@gmail.com"
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+
+        print("Email was sent")
+
+class Database:
+
+    def __init__(self, database_path):
+        # Establish a connection
+        self.connection = sqlite3.connect(database_path)
+
+
+
+    def store(self, extracted):
+        '''Writes the new data to a sqlite db'''
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
+
+
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+
+        # Query Data
+        cursor.execute(
+            "SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
+
+
+if __name__ == "__main__":
+
+    while True:
+
+        event = Event()
+
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
+        print(extracted)
+
+        if extracted != "No upcoming tours":
+            database = Database(database_path="data.db")
+            row = database.read(extracted=extracted)
+            if not row:
+                database.store(extracted=extracted)
+                email = Email()
+                email.send(message=f"""\
+Subject: New event was found
+                       
+Hey, new event was found.
+                       
+Sent from My Webscraping App
+                       
+""")
+        time.sleep(5)
