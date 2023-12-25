6e62d9867040afec97ec6235fc303996affc7cfa
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-25 22:16:11 +0200

    implemented function to book hotel
    implemented function to book hotel

--- app19-hotel-booking/main.py
+++ app19-hotel-booking/main.py
@@ -3,18 +3,26 @@
 
 import pandas
 
-df = pandas.read_csv('hotels.csv')
+df = pandas.read_csv('hotels.csv', dtype={"id":str})
 
 
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        """Books a hotel by changing its available to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """Checks if Hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 
 class ReservationTicket:
@@ -26,8 +34,9 @@ class ReservationTicket:
 
 
 print(df)
-id = input("Enter the id of the hotel: ")
-hotel = Hotel(id)
+hotel_id = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_id)
+
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
