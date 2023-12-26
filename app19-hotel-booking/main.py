90aff0a4683d1cce14ef364daa70c48eedf2dc07
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-26 08:03:53 +0200

    implement credit card checking
    Implemented Credit Card checking before booking hotel

--- app19-hotel-booking/main.py
+++ app19-hotel-booking/main.py
@@ -4,6 +4,7 @@ This uses OOP and is CLI """
 import pandas
 
 df = pandas.read_csv('hotels.csv', dtype={"id": str})
+df_cards = pandas.read_csv('cards.csv', dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -43,15 +44,34 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration,
+                     "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(
-        customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    card_number = input("Please enter the credit card number: ")
+    credit_card = CreditCard(number=card_number)
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(
+            customer_name=name, hotel_object=hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("There was a problem with your payment")
 else:
     print("Hotel is not available. ")
