866d889962534e36e03a1480360e68e1618cbf8f
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-27 13:14:11 +0200

    Added the database connection
    Added the database connection and option to load existing records from database

--- app21-student-management-system-gui/main.py
+++ app21-student-management-system-gui/main.py
@@ -1,6 +1,8 @@
 import sys
+import sqlite3
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QComboBox, QMainWindow
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
+    QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 
 
@@ -9,9 +11,11 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Student Management System")
 
+        # File and Help Menu Bar
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
 
+        # File and Help submenu items
         add_student_action = QAction("Add Student", self)
         file_menu_item.addAction(add_student_action)
 
@@ -21,8 +25,29 @@ class MainWindow(QMainWindow):
         # This needs to be used if the menu does not show
         # about_action.setMenuRole(QAction.MenuRole.NoRole)
 
+        # Add Table with 4 columns and column names
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(
+            ("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)       
+
+    def load_data(self):
+        connection = sqlite3.connect('database.db')
+        query = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(query):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))  
+        connection.close()     
+       
+        
+
 
 app = QApplication(sys.argv)
 student_system = MainWindow()
 student_system.show()
+student_system.load_data()
 sys.exit(app.exec())
