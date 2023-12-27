1d33fcfe3ebf88f8258c31f5be9937128881216e
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-27 16:35:12 +0200

    Added the menu and function to add a new record
    Added the menu and function to add a new record

--- app21-student-management-system-gui/main.py
+++ app21-student-management-system-gui/main.py
@@ -2,14 +2,18 @@ import sys
 import sqlite3
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget, QTableWidgetItem
+    QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget, \
+    QTableWidgetItem, QDialog, QVBoxLayout
 from PyQt6.QtGui import QAction
 
 
 class MainWindow(QMainWindow):
+    """Main Menu"""
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setFixedSize(800, 600)
 
         # File and Help Menu Bar
         file_menu_item = self.menuBar().addMenu("&File")
@@ -17,6 +21,7 @@ class MainWindow(QMainWindow):
 
         # File and Help submenu items
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -31,19 +36,76 @@ class MainWindow(QMainWindow):
         self.table.setHorizontalHeaderLabels(
             ("Id", "Name", "Course", "Mobile"))
         self.table.verticalHeader().setVisible(False)
-        self.setCentralWidget(self.table)       
+        self.setCentralWidget(self.table)
 
     def load_data(self):
+        """Loads data from the sql database to the tables"""
         connection = sqlite3.connect('database.db')
         query = connection.execute("SELECT * FROM students")
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(query):
             self.table.insertRow(row_number)
             for column_number, data in enumerate(row_data):
-                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))  
-        connection.close()     
-       
-        
+                self.table.setItem(row_number, column_number,
+                                   QTableWidgetItem(str(data)))
+        connection.close()
+
+    def insert(self):
+        """Loads the insert dialog"""
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    """Insert a new record dialog"""
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Add New Record")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Add Student Name
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Add Courses
+        self.course_name = QComboBox()
+        courses = ["English", "Afrikaans", "Mathematics",
+                   "Accounting", "Physics", "Computer Science", "Biology"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # Add Mobile Number
+        self.mobile_number = QLineEdit()
+        self.mobile_number.setPlaceholderText("Mobile Number")
+        layout.addWidget(self.mobile_number)
+
+        # Add Submit Button
+        button = QPushButton("Submit")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        """add student data"""
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile_number.text()
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        cursor.execute(
+            "INSERT INTO students (name, course, mobile) VALUES (?,?,?)", (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        student_system.load_data()
+        self.student_name.setText("")
+        self.mobile_number.setText("")
 
 
 app = QApplication(sys.argv)
