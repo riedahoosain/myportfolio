f9afdb510fedf129edeb4f5232497cabd9e6cf6b
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2023-12-28 22:32:37 +0200

    implemented function that stores the data into the database
    implemented function that stores the data into the database

--- app24-jobapp-flask/app.py
+++ app24-jobapp-flask/app.py
@@ -1,4 +1,5 @@
 # Job Application Web App using Flask
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,6 +9,7 @@ app.config["SECRET_KEY"] = "myapplication123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
 db = SQLAlchemy(app)
+
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,7 +29,14 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+
+        db.session.add(form)
+        db.session.commit()
 
     return render_template("index.html")
 
@@ -35,7 +44,7 @@ def index():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    
+
     # Default Port is 5000 but specifying we can change the ports to run multiple apps
     # use host to provide an IP instead
     # app.run(debug=True, host="127.0.0.1", port=5000)
