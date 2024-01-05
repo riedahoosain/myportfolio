1fa64e757175f8d7d6c1b490dad2067d845f7a03
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-06 00:23:15 +0200

    implement redirect and json
    implemented writing to json file and redirect

--- app28-url-shortener-flask/app.py
+++ app28-url-shortener-flask/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,12 +9,16 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
-        print(request.form['code'])
-        print(request.form['url'])
+        urls = {}
+        urls[request.form['code']] = {'url':request.form['url']}
+        with open("urls.json" ,'w') as url_file:
+            json.dump(urls, url_file)
+
+     
         return render_template('your_url.html', code=request.form['code'])
 
     else:
-        return "this is not valid"
+        return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
