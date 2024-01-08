d08a377fdf5b1b7231a9fae4b38d6cc905555945
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-08 15:26:54 +0200

    implement loading of short code to URL
    implement loading of short code to URL

--- app28-url-shortener-flask/app.py
+++ app28-url-shortener-flask/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+app.secret_key = 'FDSGHDFHJGJGJGHJGJHGJHGJHGJ'
 
 
 @app.route('/')
@@ -19,9 +22,20 @@ def your_url():
                 urls = json.load(urls_file)
 
         if request.form['code'] in urls.keys():
+            flash('Key exists. Please enter another URL key')
             return redirect(url_for('home'))
+        
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save(full_name)
+            urls[request.form['code']] = {'file': full_name}
 
-        urls[request.form['code']] = {'url': request.form['url']}
+
+
+        
         with open("urls.json", 'w') as url_file:
             json.dump(urls, url_file)
 
@@ -30,6 +44,17 @@ def your_url():
     else:
 
         return redirect(url_for('home'))
+    
+
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
 
 
 if __name__ == "__main__":
