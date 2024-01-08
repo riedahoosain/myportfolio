dff8409b38367a36d6d8427afea40b0d6c303e54
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-08 16:43:45 +0200

    implement Error 404
    implement Error 404

--- app28-url-shortener-flask/app.py
+++ app28-url-shortener-flask/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 import json
 import os.path
 from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
 app.secret_key = 'FDSGHDFHJGJGJGHJGJHGJHGJHGJ'
@@ -29,8 +30,8 @@ def your_url():
             urls[request.form['code']] = {'url': request.form['url']}
         else:
             f = request.files['file']
-            full_name = request.form['code'] + secure_filename(f.filename)
-            f.save(full_name)
+            full_name = request.form['code'] + secure_filename(f.filename)          
+            f.save(f"static/user_files/{full_name}")
             urls[request.form['code']] = {'file': full_name}
 
 
@@ -45,6 +46,7 @@ def your_url():
 
         return redirect(url_for('home'))
     
+    
 
 
 @app.route('/<string:code>')
@@ -55,6 +57,13 @@ def redirect_to_url(code):
             if code in urls.keys():
                 if 'url' in urls[code].keys():
                     return redirect(urls[code]['url'])
+                else:
+                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 
 if __name__ == "__main__":
