bf46e66f864cf28766a5f2616f5db3e8be2399fa
Rieda Hoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-08 21:39:01 +0200

    Update Templates

--- app28-url-shortener-flask/app.py
+++ app28-url-shortener-flask/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, \
-                  redirect, url_for, flash, abort, session
+                  redirect, url_for, flash, abort, session, jsonify
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -66,6 +66,11 @@ def redirect_to_url(code):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
 
 
 if __name__ == "__main__":
