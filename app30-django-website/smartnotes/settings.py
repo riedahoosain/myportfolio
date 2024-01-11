4a6130d6dd500e7e1e2367c2d7f73019e89115a0
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-11 10:44:39 +0200

    added a base.html

--- app30-django-website/smartnotes/settings.py
+++ app30-django-website/smartnotes/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'smartnotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
