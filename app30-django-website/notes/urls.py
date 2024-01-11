1e96f5573f8e1096fdbd6e095335c2b3c1ee041d
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-11 12:05:38 +0200

    added a new form
    added a new form

--- app30-django-website/notes/urls.py
+++ app30-django-website/notes/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('notes', views.NotesListView.as_view(), name="notes.list"),
     path('notes/', views.NotesListView.as_view(),name="notes.list"),
     path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
+    path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
+    
 ]
