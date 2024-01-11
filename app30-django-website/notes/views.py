024d8543f1c66f43907929d2f337e58bb897ed3d
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-11 13:53:41 +0200

    implement delete note option
    implement delete note option

--- app30-django-website/notes/views.py
+++ app30-django-website/notes/views.py
@@ -8,30 +8,38 @@ from .forms import NotesForm
 
 
 class NotesDeleteView(DeleteView):
+    '''Deletes a Note'''
     model = Notes
     success_url = '/smart/notes'
+    template_name = "notes/notes_delete.html"
 
 
 
 class NotesUpdateView(UpdateView):
+    '''Updates a Note'''
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    template_name = "notes/notes_form.html"
 
 
 class NotesCreateView(CreateView):
+    '''Creates a Note'''
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    template_name = "notes/notes_form.html"
 
 
 class NotesListView(ListView):
+    '''Lists the Notes'''
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
 
 
 class NotesDetailView(DetailView):
+    '''Lists Detailed Note'''
     model = Notes
     context_object_name = "note"
     template_name = "notes/notes_detail.html"
