899894818f0e9a841020f51e5e16995c4376e8d3
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-11 11:15:00 +0200

    added smart urls

--- app30-django-website/home/urls.py
+++ app30-django-website/home/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
 
 
-    path('home/', views.HomeView.as_view()),
-    path('', views.HomeView.as_view()),
-    path('home', views.HomeView.as_view()),
+    path('home/', views.HomeView.as_view(), name="home"),
+    path('', views.HomeView.as_view(), name="home"),
+    path('home', views.HomeView.as_view(),name="home"),
     path('authorized', views.AuthorizedView.as_view()),
 
 ]
