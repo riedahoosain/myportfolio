6a725f4ce457a29f786c84a283e7941e11a80013
riedahoosain <57301658+riedahoosain@users.noreply.github.com>
2024-01-11 11:42:18 +0200

    add some more styling with bootstrap
    add some more styling with bootstrap

--- app30-django-website/notes/urls.py
+++ app30-django-website/notes/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('notes', views.NotesListView.as_view(), name="notes.list"),
     path('notes/', views.NotesListView.as_view(),name="notes.list"),
-    path('notes/<int:pk>', views.NotesDetailView.as_view()),
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
 ]
